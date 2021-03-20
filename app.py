cf71074f7bb293ccec4f7533a8f4b3f87b300532
Dheeraj Yadav <dheeraj.yadav@iitgn.ac.in>
2021-03-20 21:56:36 +0530

    added create event feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import random
 
 app = Flask(__name__)
 app.secret_key = "abcd"
@@ -13,13 +14,28 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable = False, primary_key = True)
     password = db.Column(db.String(100), nullable = False)
 
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    sender_email = db.Column(db.String(100), nullable = False)
+    receiver_email = db.Column(db.String(100), nullable = False)
+    entry = db.Column(db.String(500),nullable = False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    icode = db.Column(db.Integer, nullable = False)
+    ename = db.Column(db.String(100), nullable = False)
+    edesc = db.Column(db.String(500), nullable = True)
+    eadmin = db.Column(db.String(500), nullable = False)
+    
 db.create_all()
 
 @app.route("/",methods=["POST","GET"])
 def dashboard():
     if "email" in session:
         email = session["email"]
-        return render_template("dashboard.html")
+        user_name = User.query.filter_by(email = email).first().username
+        #_notifications = Notification.query.filter_by(email = email).all()
+        return render_template("dashboard.html",user_name = user_name) #notifications = _notifications)
     else:
         return redirect("/login/")
     #return redirect("/login/")
@@ -47,8 +63,6 @@ def login():
             return render_template("login.html")
     else:
         return render_template("login.html")
-    
-
 
 @app.route("/Signup/", methods=["POST","GET"])
 def create_account():
@@ -61,6 +75,18 @@ def create_account():
         return redirect(url_for("login"))
     else:
         return render_template("signup.html")
+
+@app.route("/Createevent/", methods =["POST","GET"])
+def create_event():
+    if request.method == "GET":
+        eadmin = session["email"]
+        icode = random.randint(100000,999999)
+        new_event = Event(icode = icode,ename = request.form["event_name"],edesc = request.form["event_desc"],eadmin = eadmin)
+        db.session.add(new_event)
+        db.session.commit()
+        return redirect("/")
+    else:
+        return render_template("createevent.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
